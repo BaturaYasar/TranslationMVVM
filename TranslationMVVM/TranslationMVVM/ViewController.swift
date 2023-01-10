4aa1d3e654ca65b4808774aa7acf43e552ac1bb6
Baturay Yasar <mehmetbaturayyasar@gmail.com>
2023-01-10 19:50:55 +0100

    first commit

---
+++ TranslationMVVM/TranslationMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslationMVVM
+//
+//  Created by Mehmet Baturay Yasar on 09/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
